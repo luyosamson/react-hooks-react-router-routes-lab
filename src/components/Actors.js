19184e4518431f31493fb22edfb8f9e8073bd8f7
luyosamson <luyosamson@gmail.com>
2022-10-01 23:03:27 +0300

    Assignment completed

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,32 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+const actorsInfo=actors.map((actor)=>(
+
+<div key={actor.name}>
+
+<h4>{actor.name}</h4>
+
+<ul>
+{actor.movies.map((movie)=>(
+  <li key={movie}>
+  {movie}
+  </li>
+))}
+
+</ul>
+
+</div>
+
+))
+
+
+  return <div>
+<h1>Actors Page</h1>
+{actorsInfo}
+
+  </div>;
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,27 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const directorsInfo=directors.map((director)=>
+ <div key={director.name}>
+{director.name}
+<ul>
+{director.movies.map((movy)=>
+ <li key={movy}>
+{movy}
+
+</li>
+
+)}
+
+</ul>
+</div>
+  )
+
+  return <div><h1>Directors Page
+    {directorsInfo}
+    
+    </h1></div>;
 }
 
 export default Directors;
